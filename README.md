17588194c3c2aa68d0457f1b69e35d3e3db53dda
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2023-03-21 09:26:19 +0100

    start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # StreamingSite
+```<link rel="stylesheet/less" type="text/css" href="styles.less" />```<br>
+```<script src="https://cdn.jsdelivr.net/npm/less" ></script>```
--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+
+    <link rel="stylesheet/less" type="text/css" href="css/main.less"/>
+    <script src="https://cdn.jsdelivr.net/npm/less"></script>
+</head>
+<body>
+    <h1>Title</h1>
+
+
+    <script src="script/main.js"></script>
+</body>
+</html>
