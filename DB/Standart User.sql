7c6df45aef979cda474ffe16128be4bb8863709a
COlin Heggli <colin.heggli@gmail.com>
2023-03-29 22:39:52 +0200

    admin page user

--- DB/Standart User.sql
+++ DB/Standart User.sql
@@ -3,3 +3,13 @@ use test;
 INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Administartor', 'admin', 'test@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', TRUE);
 
 INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('User', 'user', 'test@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', TRUE);
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Manuel', 'helo', 'manu@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Ester', 'esstermaurer', 'ester.maurer@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Julian', 'herrmann', 'Julian@bbz-sh.ch', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Samy', 'smuel', 'samy@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Colin', 'colinheggli', 'colin.heggli@gmail.com.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
--- script/admin_user.js
+++ script/admin_user.js
@@ -1,6 +1,6 @@
 $(function () {
 
-        var JSONData = [{name: "hallo", username: "memo"}]
+        var JSONData = []
         const base_url = window.location.origin;
 
         const getUser = async (api) => {
@@ -17,9 +17,9 @@ $(function () {
 
             for (let i = 0; i < JSON.length; i++) {
                 if (JSON[i].admin == 1) {
-                    JSON[i].admin = true
+                    JSON[i].admin = "fa-solid fa-check IsAdmin"
                 } else {
-                    JSON[i].admin = false
+                    JSON[i].admin = "fa-solid fa-xmark IsNotAdmin"
                 }
 
                 JSONData.push({
@@ -46,10 +46,6 @@ $(function () {
                 computed: {
                     filteredUsers() {
                         return this.users.filter(p => {
-                            // return true if the product should be visible
-
-                            // in this example we just check if the search string
-                            // is a substring of the product name (case insensitive)
                             return p.username.toLowerCase().indexOf(this.search.toLowerCase()) != -1;
                         });
                     }
@@ -58,5 +54,47 @@ $(function () {
         }
         console.log(getUser('test'))
     }
-)
-;
+);
+// Make the DIV element draggable:
+// dragElement(document.getElementById("mydiv"));
+//
+// function dragElement(elmnt) {
+//     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+//     if (document.getElementById(elmnt.id + "header")) {
+//         // if present, the header is where you move the DIV from:
+//         document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+//     } else {
+//         // otherwise, move the DIV from anywhere inside the DIV:
+//         elmnt.onmousedown = dragMouseDown;
+//     }
+//
+//     function dragMouseDown(e) {
+//         e = e || window.event;
+//         e.preventDefault();
+//         // get the mouse cursor position at startup:
+//         pos3 = e.clientX;
+//         pos4 = e.clientY;
+//         document.onmouseup = closeDragElement;
+//         // call a function whenever the cursor moves:
+//         document.onmousemove = elementDrag;
+//     }
+//
+//     function elementDrag(e) {
+//         e = e || window.event;
+//         e.preventDefault();
+//         // calculate the new cursor position:
+//         pos1 = pos3 - e.clientX;
+//         pos2 = pos4 - e.clientY;
+//         pos3 = e.clientX;
+//         pos4 = e.clientY;
+//         // set the element's new position:
+//         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+//         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+//     }
+//
+//     function closeDragElement() {
+//         // stop moving when mouse button is released:
+//         document.onmouseup = null;
+//         document.onmousemove = null;
+//     }
+// }
