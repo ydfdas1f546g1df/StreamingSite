3ab950b9517732a1217211e3ba7dc7b8f862abf2
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2023-04-03 14:13:55 +0200

    change api

--- api/user.php
+++ api/user.php
@@ -3,10 +3,34 @@
  * @var mysqli $mysqli
  */
 
-
 include_once("db_connect.php");
 
-$stmt = $mysqli->prepare('SELECT * FROM tbl_users');
+
+if(isset($_GET['search'])){
+    $search = $_GET['search'];
+    $regemail = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,5})$/im';
+    $reguser = '/^@.*$/';
+
+    if(preg_match($regemail, $search)){
+        $stmt = $mysqli->prepare('SELECT id, username, admin, name, bio, created FROM tbl_users WHERE email LIKE (?)');
+        $search = '%'.$search.'%';
+        $stmt->bind_param('s', $search);
+    }elseif (preg_match($reguser, $search)){
+        $stmt = $mysqli->prepare('SELECT id, username, admin, name, bio, created FROM tbl_users WHERE username LIKE (?)');
+        $search = ltrim($search, '@');
+        $search = '%'.$search.'%';
+        $stmt->bind_param('s', $search);
+    }else{
+        $stmt = $mysqli->prepare('SELECT id, username, admin, name, bio, created FROM tbl_users WHERE name LIKE (?)');
+        $search = '%'.$search.'%';
+        $stmt->bind_param('s', $search);
+    }
+}else {
+
+    $stmt = $mysqli->prepare('SELECT id, username, admin, name, bio, created FROM tbl_users');
+
+}
+
 $stmt->execute();
 $result = $stmt->get_result();
 
