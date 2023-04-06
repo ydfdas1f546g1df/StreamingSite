d0833798533c73ecb668c254c6d73c1d34317c4e
COlin Heggli <colin.heggli@gmail.com>
2023-04-06 22:27:28 +0200

    fixing registration api

--- DB/Standart User.sql
+++ DB/Standart User.sql
@@ -1,21 +1,43 @@
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Administartor', 'admin', 'test@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', TRUE);
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Administartor', 'admin', 'test@test.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', TRUE);
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('User', 'user', 'test@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', TRUE);
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('12973h9ue9r7fgh934un39fuwne9rn', 1, '2023-12-12');
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Manuel', 'helo', 'manu@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('User', 'user', 'test@test.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', TRUE);
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Ester', 'esstermaurer', 'ester.maurer@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('12973h9ue9r7fgh934un39furkazrn', 2, '2023-12-12');
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Julian', 'herrmann', 'Julian@bbz-sh.ch', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Manuel', 'helo', 'manu@test.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Samy', 'smuel', 'samy@test.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('12973h9ue9rsdfgvijsndvosssodvn', 3, '2023-12-12');
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Colin', 'colinheggli', 'colin.heggli@gmail.com.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Ester', 'esstermaurer', 'ester.maurer@test.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Niklaus', 'niklaus', 'niklaus@gmail.com.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('9876g90q8z34b5rgpiajnsdvpiaerg', 4, '2023-12-12');
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Rezze', 'dominik', 'memo@gmail.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Julian', 'herrmann', 'Julian@bbz-sh.ch', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
 
-INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Karin', 'laurenz', 'desises@mail.com', '7ce1add0d288b35e45c616c518379675ffd3cb1c80b3f31f314d01e509057f81', false);
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('ip0ounP9WUERNRASFAFBrtrrrrrhgG', 5, '2023-12-12');
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Samy', 'smuel', 'samy@test.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
+
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('9ozbodiivjbaOUSZDVFOUsASHBDFOI', 6, '2023-12-12');
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Colin', 'colinheggli', 'colin.heggli@gmail.com.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
+
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('alojhbdofizuqbosarfiijbdsflkhb', 7, '2023-12-12');
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Niklaus', 'niklaus', 'niklaus@gmail.com.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
+
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('alojhbdofizuqbosasldhijksudnvp', 8, '2023-12-12');
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Rezze', 'dominik', 'memo@gmail.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
+
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('alojhbdofizuqbosasldhsafdbdbsd', 9, '2023-12-12');
+
+INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Karin', 'laurenz', 'desises@mail.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', false);
+
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('ijouhiuboubsdofsssosdnfosundfo', 10, '2023-12-12');
 
 INSERT INTO tbl_users (name, username, email, passwordHash, admin) VALUE ('Colin Heggli', 'colin', 'colin.heggli@gmail.com', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', true);
+
+INSERT INTO tbl_apitoken (id, user, expire) VALUE ('alojhsdvsddvsdvsdvsdvsafdbdbsd', 11, '2023-12-12');
