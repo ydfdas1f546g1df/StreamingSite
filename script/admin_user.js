1a16bf91bda53e0d15d867edaf4ba76b3d810a2a
COlin Heggli <colin.heggli@gmail.com>
2023-04-07 09:50:45 +0200

    User admin Page

--- script/admin_user.js
+++ script/admin_user.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function loadUsers() {
 
         var JSONData = []
         const base_url = window.location.origin;
@@ -18,15 +18,19 @@ $(function () {
             for (let i = 0; i < JSON.length; i++) {
                 if (JSON[i].admin == 1) {
                     JSON[i].admin = "fa-solid fa-check IsAdmin"
+                    var isAdmin = 1;
+
                 } else {
                     JSON[i].admin = "fa-solid fa-xmark IsNotAdmin"
+                    var isAdmin = 0;
                 }
 
                 JSONData.push({
                     id: JSON[i].id,
                     admin: JSON[i].admin,
+                    IsAdmin: isAdmin,
                     name: JSON[i].name,
-                    username: JSON[i].name,
+                    username: JSON[i].username,
                     email: JSON[i].email,
                     passwordHash: JSON[i].passwordHash,
                     bio: JSON[i].bio,
@@ -51,6 +55,25 @@ $(function () {
                         if (confirm("Are you sure you want to delete the user: " + $(e.target).parent().parent().find(".user-list-el-username").text())) {
                             $(e.target).parent().parent().remove()
                         }
+                    },
+                    editUser(e) {
+
+                        var target = $(e.target)
+                        var el = $(e.target).parent().parent()
+                        if (el.find(".user-list-el-username").attr("contenteditable") === "false" || el.find(".user-list-el-username").attr("contenteditable") == undefined) {
+                            target.attr("class", "fas fa-save edit-btn user-list-btn")
+                            el.find(".user-list-el-username").attr("contenteditable", "true")
+                            el.find(".user-list-el-name").attr("contenteditable", "true")
+                            el.find(".user-list-el-email").attr("contenteditable", "true")
+                            el.find(".user-list-el-admin").find("i").css("cursor", "pointer").attr("status", "1")
+                        } else {
+                            target.attr("class", "fa-solid fa-pen-to-square edit-btn user-list-btn")
+                            el.find(".user-list-el-username").attr("contenteditable", "false")
+                            el.find(".user-list-el-name").attr("contenteditable", "false")
+                            el.find(".user-list-el-email").attr("contenteditable", "false")
+                            el.find(".user-list-el-admin").find("i").css("cursor","default").attr("status", "0")
+                        }
+
                     }
                 },
                 computed: {
