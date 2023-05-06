a9217d9543bd7b70502d0302f31f1a723f5f02ae
COlin Heggli <colin.heggli@gmail.com>
2023-05-06 10:46:41 +0200

    Home Page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include './template/index.php';
 $homeContent = '
-<main>
+<main id="home-main">
     <div class="quick-search" id="quick-search">
         <a :href="\'/pages/search/?search=\' + quick"  class="quick-search-el" v-for="quick in quicks">{{ quick }}</a>
     </div>
@@ -55,34 +55,34 @@ $homeContent = '
             <a href="/pages/popular.php" class="home-el-all">View all</a>
         </span>
         <div id="home-cat-1" class="wrapper home-cat">
-            <a v-for="media in Media" :href="\'/stream/\' + media.medianame" class="home-el"
-               :title="media.name + \', watch it now for free and in full length.\'">
-                <img :src="media.coverSrc" alt="cover" class="cover">
-                <span class="cover-title"><span>{{ media.name }}</span></span>
+            <a v-for="po in pupular" :href="\'/stream/\' + po.name" class="home-el"
+               :title="po.showName + \', watch it now for free and in full length.\'">
+                <img :src="\'/dist/img/\' + po.name + \'.jpg\'" alt="cover" class="cover">
+                <span class="cover-title"><span>{{ po.showName }}</span></span>
             </a>
         </div>
     </div>
     <div class="home-cat-parent">
-        <span class="home-el-title">Recent watched
-            <a href="/user/watched/?u='. $username .'" class="home-el-all">View all</a>
+        <span class="home-el-title">Recent Watched
+            <a href="/user/watched/" class="home-el-all">View all</a>
         </span>
-        <div id="home-cat-2" class="wrapper home-cat">
-            <a v-for="media in Media" :href="\'/stream/\' + media.medianame" class="home-el"
-               :title="media.name + \', watch it now for free and in full length.\'">
-                <img v-bind:src="media.coverSrc" alt="cover" class="cover">
-                <span class="cover-title"><span>{{ media.name }}</span></span>
+        <div class="wrapper home-cat">
+            <a v-for="wd in watched" :href="\'/stream/\' + wd.name + \'/season-\' + wd.season + \'/episode-\' + wd.episode" class="home-el"
+               :title="wd.showName + \', watch it now for free and in full length.\'">
+                <img :src="\'/dist/img/\' + wd.name + \'.jpg\'" alt="cover" class="cover">
+                <span class="cover-title"><span>{{ wd.showName }} &nbsp;<span class="episode-nth">S{{ wd.season }} &nbsp;E{{ wd.episode }}</span></span></span>
             </a>
         </div>
     </div>
     <div class="home-cat-parent">
-        <span class="home-el-title" id="latest">Latest uploads
-            <a href="/user/watchlist/?u='. $username .'" class="home-el-all">View all</a>
+        <span class="home-el-title">Latest Uploads
+            <a href="/user/watched/" class="home-el-all">View all</a>
         </span>
-        <div id="home-cat-3" class="wrapper home-cat">
-            <a v-for="media in Media" :href="\'/stream/\' + media.medianame" class="home-el"
-               :title="media.name + \', watch it now for free and in full length.\'">
-                <img v-bind:src="media.coverSrc" alt="cover" class="cover">
-                <span class="cover-title"><span>{{ media.name }}</span></span>
+        <div class="wrapper home-cat">
+            <a v-for="up in uploads" :href="\'/stream/\' + up.name + \'/season-\' + up.season + \'/episode-\' + up.episode" class="home-el"
+               :title="wd.showName + \', watch it now for free and in full length.\'">
+                <img :src="\'/dist/img/\' + up.name + \'.jpg\'" alt="cover" class="cover">
+                <span class="cover-title"><span>{{ up.showName }} &nbsp;<span class="episode-nth">S{{ up.season }} &nbsp;E{{ up.episode }}</span></span></span>
             </a>
         </div>
     </div>
