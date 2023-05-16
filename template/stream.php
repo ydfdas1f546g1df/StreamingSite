1505024842c8818bc855903eed1762ddf30b1111
COlin Heggli <colin.heggli@gmail.com>
2023-05-16 23:57:10 +0200

    Stream

--- template/stream.php
+++ template/stream.php
@@ -10,16 +10,19 @@ $streamTopMain = '
     <div class="info-column">
         <strong class="series-title" id="SeriesName">{{ series.showName }}</strong>
         <span class="series-desc">{{ series.desc }}</span>
+        <div class="series-info-parent">
+        
         <span class="series-info"><strong>Regisseur: </strong><span>{{ series.reg }}</span></span>
         <span class="series-info"><strong>Language: </strong><span>{{ series.lang }}</span></span>
         <span class="series-info"><strong>Watchlist: </strong><span id="top-watchlist">{{ series.watchlist }}</span></span>
         <span class="series-info"><strong>Watched: </strong><span id="top-watched">{{ series.watched }}</span></span>
+</div>
     </div>
     <div class="watchlist-column">
         <div>
             <a id="watchNow" :href="\'/stream/\' + series.name + \'/season-1/episode-1\'">Watch now</a>
             <span :class="\'watchlist-add \' + series.OW" :title="\'Add \' + series.showName + \' to your your watchlist, now.\'" :status="onWatchlist" id="watchlist" @click="watchlist"><i class="fa-solid fa-list-ul"></i> Watchlist</span>
-            <span class="share"><i class="fa-solid fa-share-from-square"></i> Share</span>
+            <a class="share" :href="\'https://twitter.com/intent/tweet?hashtags=stream%2Cseries&original_referer=https%3A%2F%2Fstreamingsite.com&text=\' + series.showName + \', watch it now on the best StreamingSite in the world! https://stramingsite/stream/\' + series.name + \'\'"><i class="fa-solid fa-share-from-square"></i> Share</a>
         </div>
     </div>
     <div class="background first"   :style="\'background-image: url(/data/\' + series.name + \'/\' + series.name + \'.jpg);\'"></div>
